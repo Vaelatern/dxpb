90e0d37856534c3cff63a385f32b6a375f790a87
Toyam Cox <Vaelatern@gmail.com>
2017-10-08 15:48:28 -0400

    Fix prologue to not include path elements

--- src/dxpb-common.c
+++ src/dxpb-common.c
@@ -8,6 +8,10 @@
 #define _POSIX_C_SOURCE 200809L
 
 #include <stdio.h>
+#include <libgen.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
 #include "dxpb-common.h"
 
 #define VERSION "alpha-0.1.0"
@@ -15,7 +19,10 @@
 void
 prologue(const char *argv0)
 {
-	printf("DXPB: Distributed XBPS Package Builder\n%s %s\nCopyright 2016-2017, Toyam Cox\nThis is free software with ABSOLUTELY NO WARRANTY.\nFor license details, pass the -L flag\nCompiled: %s %s\n\n", argv0, VERSION, __DATE__, __TIME__);
+	char *arg = strdup(argv0);
+	assert(arg);
+	printf("DXPB: Distributed XBPS Package Builder\n%s %s\nCopyright 2016-2017, Toyam Cox\nThis is free software with ABSOLUTELY NO WARRANTY.\nFor license details, pass the -L flag\nCompiled: %s %s\n\n", basename(arg), VERSION, __DATE__, __TIME__);
+	free(arg);
 }
 
 void
