1b93db2f6312368cd07a20591dae2d629d6325d1
Toyam Cox <Vaelatern@gmail.com>
2017-10-09 10:38:35 -0400

    Prettify defaults and change default file endpoint

--- src/dxpb-common.h
+++ src/dxpb-common.h
@@ -9,17 +9,17 @@ void	print_license(void);
 #define DEFAULT_SSLDIR "/etc/dxpb/curve/"
 
 #define DEFAULT_IMPORT_ENDPOINT "ipc:///var/run/dxpb/pkgimport.sock"
-#define DEFAULT_FILE_ENDPOINT "ipc:///var/run/dxpb/pkgfiler.sock"
-#define DEFAULT_GRAPH_ENDPOINT "tcp://127.0.0.1:5195"
+#define DEFAULT_FILE_ENDPOINT   "tcp://127.0.0.1:5196"
+#define DEFAULT_GRAPH_ENDPOINT  "tcp://127.0.0.1:5195"
 
 #define DEFAULT_DBPATH "/var/cache/dxpb/pkgs.db"
 
 #define DEFAULT_STAGINGDIR "/var/lib/dxpb/staging/"
-#define DEFAULT_LOGDIR "/var/lib/dxpb/logs/"
-#define DEFAULT_REPODIR "/var/lib/dxpb/pkgs/"
+#define DEFAULT_LOGDIR     "/var/lib/dxpb/logs/"
+#define DEFAULT_REPODIR    "/var/lib/dxpb/pkgs/"
 
 #define DEFAULT_REPOPATH "./"
-#define DEFAULT_XBPS_SRC "./xbps-src"
 
+#define DEFAULT_XBPS_SRC "./xbps-src"
 #define	DEFAULT_MASTERDIR "./masterdir"
-#define	DEFAULT_HOSTDIR "./hostdir"
+#define	DEFAULT_HOSTDIR   "./hostdir"
