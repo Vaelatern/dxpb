c7482e3a6c33636ec3d51014256e68a028751d3f
Toyam Cox <Vaelatern@gmail.com>
2017-10-09 10:27:42 -0400

    Problem: dxpb-common.h is important, yet untracked
    Solution: Commit it, with an appropiate .gitignore change.

---
+++ src/dxpb-common.h
@@ -0,0 +1,25 @@
+// Remove the following to cause errors when ssldir is ready to be rolled out.
+#define SSLDIR_UNUSED(x) (void)x
+
+void	prologue(const char *);
+void	print_license(void);
+
+
+
+#define DEFAULT_SSLDIR "/etc/dxpb/curve/"
+
+#define DEFAULT_IMPORT_ENDPOINT "ipc:///var/run/dxpb/pkgimport.sock"
+#define DEFAULT_FILE_ENDPOINT "ipc:///var/run/dxpb/pkgfiler.sock"
+#define DEFAULT_GRAPH_ENDPOINT "tcp://127.0.0.1:5195"
+
+#define DEFAULT_DBPATH "/var/cache/dxpb/pkgs.db"
+
+#define DEFAULT_STAGINGDIR "/var/lib/dxpb/staging/"
+#define DEFAULT_LOGDIR "/var/lib/dxpb/logs/"
+#define DEFAULT_REPODIR "/var/lib/dxpb/pkgs/"
+
+#define DEFAULT_REPOPATH "./"
+#define DEFAULT_XBPS_SRC "./xbps-src"
+
+#define	DEFAULT_MASTERDIR "./masterdir"
+#define	DEFAULT_HOSTDIR "./hostdir"
