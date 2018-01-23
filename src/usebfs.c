e608b20a9144b689d389c2872c3fdba3a675a508
Toyam Cox <Vaelatern@Voidlinux.eu>
2018-01-22 22:49:17 -0500

    Remove unused function

--- src/usebfs.c
+++ src/usebfs.c
@@ -1,5 +1,4 @@
 #define _POSIX_C_SOURCE 200809L
-// The above line is part of the tests used. Please do not alter.
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -18,29 +17,4 @@ int main(void)
 	}
 	free(retVal);
 	retVal = NULL;
-	assert(bfs_find_file_in_subdir("./", "usebfs.c", NULL));
-	assert(bfs_find_file_in_subdir("../../../", "usebfs.c", NULL));
-	assert(!bfs_find_file_in_subdir("./", "doesnotexist.c", NULL));
-	assert(!bfs_find_file_in_subdir("../../../", "doesnotexist.c", NULL));
-
-	char *end = NULL;
-	char goal[32];
-	int fd = bfs_find_file_in_subdir("./", "usebfs.c", &end);
-	assert(end == NULL);
-	free(end);
-	end = NULL;
-	ssize_t rdin = read(fd, goal, 32);
-	assert(rdin == 32);
-	assert(strcmp("#define _POSIX_C_SOURCE 200809L\n", goal) == 0);
-	close(fd);
-
-	fd = bfs_find_file_in_subdir("../", "usebfs.c", &end);
-	assert(strcmp(end, "/src") == 0);
-	free(end);
-	end = NULL;
-	assert(fd > 2);
-	rdin = read(fd, goal, 32);
-	assert(rdin == 32);
-	assert(strcmp("#define _POSIX_C_SOURCE 200809L\n", goal) == 0);
-	close(fd);
 }
