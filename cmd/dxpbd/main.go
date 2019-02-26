8ff9f5de65bb3cef061a6343c7a09fbbd478a943
Toyam Cox <Vaelatern@voidlinux.org>
2019-02-26 16:10:19 -0500

    Problem: Config gets out of whack
    Solution: bring config somewhere we must see it

--- cmd/dxpbd/main.go
+++ cmd/dxpbd/main.go
@@ -4,15 +4,29 @@ import (
 	"log"
 	"net"
 
+	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
 
 	"github.com/dxpb/dxpb/internal/http"
 	"github.com/dxpb/dxpb/internal/irc"
 )
 
+func startConfig() {
+	pflag.BoolP("debug", "d", false, "Debug mode")
+	pflag.Parse()
+	viper.BindPFlags(pflag.CommandLine)
+	viper.SetConfigName("dxpbd")
+	viper.AddConfigPath("/etc/dxpb")
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig()
+	if err != nil {
+		log.Panicf("Could not read in configuration file: %s\n", err)
+	}
+}
+
 func main() {
 	log.Println("hello!")
-	Start()
+	startConfig()
 
 	ircClient, err := irc.New()
 	if err != nil {
