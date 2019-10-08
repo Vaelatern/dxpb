de9d3949928ddce383708d2c4d43c6afbc4fef16
Robert Lowry <bobertlo@gmail.com>
2019-10-08 09:35:43 -0500

    Problem: First argument ignored
    Solution: Prepend the command to the list of arguments

--- internal/shell/script.go
+++ internal/shell/script.go
@@ -8,7 +8,7 @@ import (
 func Exec(cmd string, args []string) (*exec.Cmd, io.ReadCloser, error) {
 	command := exec.Command(cmd)
 	stdPipe, err := command.StdoutPipe()
-	command.Args = args
+	command.Args = append([]string{cmd}, args...)
 	err = command.Start()
 	return command, stdPipe, err
 }
