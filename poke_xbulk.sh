59197f72d5a08fb9961ff0ae77ce3d2e52d1a4b3
Robert Lowry <bobertlo@gmail.com>
2019-10-09 09:01:50 -0500

    Problem: Can't get started by reading documentation
    Solution: add example configuration files for working package builders
    
    Solution: add poke_xbulk.sh script to be called by the builder

---
+++ poke_xbulk.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# This is a wrapper script to make calls to xbps-bulk-builder.sh simpler.
+# the bootstrap and target architecture are mandatory. For a native build
+# (the same target arch as the host) you MUST set target to "native" (or
+# leave it blank.)
+
+USAGE="Usage: $0 [-C workdir] [-h hostdir] bootstrap-arch [target-arch]"
+
+WORKDIR=$(pwd)
+
+# FIXME: hack for testing
+START=$(pwd)
+
+while getopts C:c:h: OPT; do
+	case "$OPT" in
+	C)
+		WORKDIR="$OPTARG"
+		;;
+	h)
+		HOSTDIR="$OPTARG"
+		;;
+	c)
+		XCONF=-c $(realpath "$OPTARG")
+		;;
+	\?)
+		prtinf "%s\n" "$USAGE"
+		exit 1
+		;;
+	esac
+done
+shift $((OPTIND - 1))
+
+if [ -z "$HOSTDIR" ]; then
+	HOSTDIR="$WORKDIR/hostdir"
+fi
+
+if [ -z "$1" ]; then
+	printf "%s\n" "$USAGE"
+	exit 1
+fi
+
+BOOTSTRAP="$1"
+TARGET="${2:-native}"
+
+echo RUNNING XBULK BUILDER
+echo BOOTSTRAP=$BOOTSTRAP
+echo TARGET=$TARGET
+echo WORKDIR=$WORKDIR
+echo HOSTDIR=$HOSTDIR
+
+if [ -n "$XCONF" ]; then
+	XCONF="-c $XCONF"
+elif [ -f "/etc/dxpb/xbps.conf" ]; then
+	XCONF="-c /etc/dxpb/xbps.conf"
+fi
+
+mkdir -p $WORKDIR
+cd $WORKDIR
+
+$START/assets/builder/xbps-bulk-builder.sh -a $TARGET -B $BOOTSTRAP \
+    $XCONF -N -h $HOSTDIR -R $HOSTDIR/binpkgs
